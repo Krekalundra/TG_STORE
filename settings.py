91a99df2b90ac9a201da09491d9b7f6ac8274119
Alex <workmail@mail.work>
2025-01-25 06:52:38 +0000

    Установили админку

---
+++ settings.py
@@ -0,0 +1,9 @@
+ALLOWED_HOSTS = ['158.255.6.226', 'django146299.hostkey.in', 'localhost']
+INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",  # именно так
+    "django.contrib.admin",
+    # ... остальные приложения ...
+]
+X_FRAME_OPTIONS = "SAMEORIGIN"
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
